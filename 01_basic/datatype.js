347c64b5c64110be3c6d7cd39db300894d9ec80f
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-07 18:09:02 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+let name = "aman"
+let age  = 18
+let isLoggedIn = false
+
+//data type 
+//number => 2 to the power 53 
+// bigint
+//string => "  "
+//boolean => true and false
+//null => stand alone value
+//undefined => value not define
+//symbol => unique
+
+
+// object
+
+console.log(typeof null);
