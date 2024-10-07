37f6a6f2e199badfc910d3ff15dfb1e7418b5f70
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-07 17:52:38 +0000

    lecture4 complete

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 124578
+let emailId = "vaman7409@gmail.com"
+var password  = "12345"
+accountCity = "lucknow"
+let accountState;
+
+// accountId = 2  can't change
+
+emailId = "maanmerijaan@123"
+password = "1245"
+accountCity = "jaipur"
+// console.log(emailId);
+
+console.table([emailId,password,accountCity,accountState]);
