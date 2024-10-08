f22ea1bb72019fd67a892fcd46dcc7ce840794d6
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-08 16:16:25 +0000

    conversion

---
+++ 01_basic/03_conversionOp.js
@@ -0,0 +1,27 @@
+let score = false
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "aman" => NaN
+// true => 1 or false => 0
+// null => 0
+// undefined => NaN
+
+let isLogged = "0"
+let valueInBoolean = Boolean(isLogged)
+// console.log(valueInBoolean);
+
+// 1 => true or 0=> false
+// " " => false
+// "anything except empty" => true
+
+let someNumber = true
+let valueInString = String(someNumber)
+console.log(valueInString)
+console.log(typeof valueInString);
