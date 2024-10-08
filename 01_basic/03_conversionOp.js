9b21f267f7ebafa73339be00805578902e75fa5c
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-08 16:46:00 +0000

    operation in datatype

--- 01_basic/03_conversionOp.js
+++ 01_basic/03_conversionOp.js
@@ -23,5 +23,38 @@ let valueInBoolean = Boolean(isLogged)
 
 let someNumber = true
 let valueInString = String(someNumber)
-console.log(valueInString)
-console.log(typeof valueInString);
+// console.log(valueInString)
+// console.log(typeof valueInString);
+
+
+// *************************operation*********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let srt1 = "hello"
+let str2 = " aman"
+let str3 = srt1+str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+2+"2"+1+2);
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
