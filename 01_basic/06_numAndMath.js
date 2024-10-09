2adb043d9d31f9d9538bd26ca4321b6047d9049c
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-09 08:36:53 +0000

    num and math

---
+++ 01_basic/06_numAndMath.js
@@ -0,0 +1,36 @@
+const score = 33
+
+const balance = new Number(100)
+// console.log(balance);
+
+// const valueInString = balance.toString()
+// console.log(valueInString);
+// console.log(typeof valueInString)
+
+// console.log(balance.toFixed(1))
+
+const anotherNum = 128.481
+// console.log(anotherNum.toPrecision(3))
+
+const hunders = 1000000
+// console.log(hunders.toLocaleString());
+// console.log(hunders.toLocaleString('en-IN'))
+
+
+// +++++++++++++++++++  Maths  +++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(100.1))
+// console.log(Math.pow(2,3))
+// console.log(Math.max(4,3,2,0,-4,2))
+
+
+// console.log(Math.random())
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
