44d8edf40a44194c972df61989791231dbfd690d
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-10 07:10:28 +0000

    array1

---
+++ 02_basic/02_array.js
@@ -0,0 +1,29 @@
+const marvel_heros  = ["Ironman","Spider-man","Thor"]
+const dc_heros  = ["Super-man","Bat-man","Aqua-man"]
+
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][2]);
+
+// const new_heros = marvel_heros.concat(dc_heros)
+// console.log(new_heros);
+
+
+// const all_heros = [...marvel_heros,...dc_heros]
+// console.log(all_heros);
+
+// const new_arr = [1,2,3,[5,8,9],4,7,[8,9,10]]
+// console.log(new_arr.flat(Infinity))
+
+console.log(Array.isArray("Aman","verma"))
+console.log(Array.from("aman"))
+
+
+let a = [1,3,2]
+let b = "aman"
+let c = 99
+let d = 1
+
+console.log(Array.of(a,b,c,d))
