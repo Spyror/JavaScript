1a944ba1af78b1cf9298439024acafc5c51fba32
Spyror <130476147+Spyror@users.noreply.github.com>
2024-10-10 07:10:42 +0000

    array2

---
+++ 02_basic/01_array.js
@@ -0,0 +1,41 @@
+// Arrays
+
+const myArr = [1,2,3,4,5]
+// console.log(myArr[3]);
+const arr2 = new Array(1,2,3,4,5)
+
+//Methods in Array
+
+// myArr.push(6,5,3,2,5)
+// console.log(myArr)
+
+// myArr.pop()
+// myArr.pop()
+
+
+// myArr.unshift(1,2,3)
+// myArr.shift()
+
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr);
+
+
+// slice , splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr)
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr)
+
+console.log(myn2);
+
+
